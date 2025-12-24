4be02c71c0bbebcf6729fb8954177d029020b1a8
AtlasShiny <Mannyoso07@gmail.com>
2025-12-24 01:08:48 -0500

    framework for future data structuring/cleaning

---
+++ data/data_cleanup.py
@@ -0,0 +1,3 @@
+# import various methods of accessing data into the universial stucture outlined in data_structure_template.md (located in this folder)
+
+# once a standard data format is established, finish this script to turn the raw data into clean, uniform data
