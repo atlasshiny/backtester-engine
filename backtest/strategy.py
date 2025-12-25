0ac6af33424b02685df25a63910c681cdb4f691e
AtlasShiny <Mannyoso07@gmail.com>
2025-12-25 13:08:24 -0500

    added on_start and on_finish method template

--- backtest/strategy.py
+++ backtest/strategy.py
@@ -6,6 +6,14 @@ class Strategy():
     def __init__(self):
         pass
 
+    def on_start(self):
+        """Override this for each individual subclass if needed"""
+        raise NotImplementedError("on_start must be implemented by subclass.")
+
+    def on_finish(self):
+        """Override this for each individual subclass if needed"""
+        raise NotImplementedError("on_finish must be implemented by subclass.")
+
     def check_condition(self, event: tuple) -> Literal["BUY", "SELL", "HOLD"]:
         """Override this for each individual subclass"""
         raise NotImplementedError("check_condition must be implemented by subclass.")
