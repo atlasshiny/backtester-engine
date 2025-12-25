11c47bfb39aa972ed85c362020ceea05e886832b
AtlasShiny <Mannyoso07@gmail.com>
2025-12-25 18:39:41 -0500

    fixed error in methods that led to incorrect calculations and outcomes

--- backtest/portfolio.py
+++ backtest/portfolio.py
@@ -39,12 +39,13 @@ class Portfolio():
         value = self.cash + sum(pos['amount'] * current_price for pos in self.positions.values())
         self.value_history.append(value)
 
-    def log_trade(self, side: Literal["BUY", "SELL", "HOLD"], amount: int, symbol_or_name: str, price: int):
+    def log_trade(self, side: Literal["BUY", "SELL", "HOLD"], amount: int, symbol_or_name: str, price: int, comment: str):
         trade = {
             "symbol": symbol_or_name,
             "side": side,
             "qty": amount,
-            "price": price
+            "price": price,
+            "comment": comment
         }
         self.trade_log.append(trade)
 
@@ -52,11 +53,13 @@ class Portfolio():
         price = event[4] # using the "high" of a day's worth of data
         match action[0]:
             case "BUY":
-                if self.cash > event[4]: # check if there is enough money to buy (this is checking the "high" of the day to make the trade)
-                    self.cash = self.cash - event[4]
+                total_cost = event[4] * action[1]
+                if self.cash >= total_cost:
+                    self.cash -= total_cost
                     self.add_position(symbol_or_name=symbol_or_name, amount=action[1], price=price)
                     self.log_trade(side=action[0], amount=action[1], symbol_or_name=symbol_or_name, price=price)
-                    # print(f"Action executed : BUY {action[1]} of {symbol_or_name} at {price}")
+                else: 
+                    self.log_trade(side=action[0], amount=0, symbol_or_name=symbol_or_name, price=None, comment="Insufficint Funds")
             case "HOLD":
                 self.log_trade(side=action[0], amount=action[1], symbol_or_name=symbol_or_name, price=price)
                 pass
@@ -65,8 +68,10 @@ class Portfolio():
                 self.cash += amount_to_sell * price
                 self.remove_position(symbol=symbol_or_name, amount=amount_to_sell, price=price)
                 self.log_trade(side=action[0], amount=action[1], symbol_or_name=symbol_or_name, price=price)
-                # print(f"Action executed : SELL {amount_to_sell} of {symbol_or_name} at {price}")
         self.update_value_history(event[4])
 
     def portfolio_value_snapshot(self, event: float) -> float:
-        return self.cash + (self.positions['SYNTH']['amount'] * event)
+        if not self.positions:
+            return self.cash
+        else:
+            return self.cash + sum(pos['amount'] * event for pos in self.positions.values())
