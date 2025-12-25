322afc098756de15be16fe09ae6ff9b2fd7ef2b1
AtlasShiny <Mannyoso07@gmail.com>
2025-12-25 12:33:01 -0500

    refactored for use with the new portfolio class

--- backtest/engine.py
+++ backtest/engine.py
@@ -1,17 +1,40 @@
 import pandas as pd
 from .strategy import Strategy
+from .portfolio import Portfolio
 
 class BacktestEngine():
-    def __init__(self, strategy: Strategy, data_set: pd.DataFrame):
+    def __init__(self, strategy: Strategy, portfolio: Portfolio, data_set: pd.DataFrame):
         self.strategy = strategy
         self.data_set = data_set
+        self.portfolio = portfolio
+        self.successful_trades = pd.DataFrame()
+        self.unsuccessful_trades = pd.DataFrame()
         pass
 
     def run(self):
         for event in self.data_set.itertuples():
-            if self.strategy.check_condition(event=event) == True:
-                self.strategy.execute(event=event)
-                print("Action executed")
-            else:
-                print("No action taken")
-            pass
+            print(event)
+            print("position count")
+            print(self.portfolio.position)
+            print("cash")
+            print(self.portfolio.cash)
+            match self.strategy.check_condition(event=event):
+                case "BUY":
+                    if self.portfolio.cash > event[3]: # check if there is enough money to buy (this is checking the "high" of the day to make the trade)
+                        self.portfolio.cash = self.portfolio.cash - event[3]
+                        self.portfolio.position += 1 # keep a simple count of positions, no information yet
+                        print("Action executed : Buying")
+                    else:
+                        print("No Action taken : Not enough money to buy in")
+                    pass
+                case "HOLD":
+                    print("Action executed : Holding")
+                    pass
+                case "SELL":
+                    if self.portfolio.position >= 1: # check if there is any positions to sell
+                        self.portfolio.position -= 1 # remove 1 from the count of positions, add some random amount of money in order for the system to work for testing purposes
+                        self.portfolio.cash += 100
+                        print("Action executed : Selling")
+                    else:
+                        print("No Action taken : No positions exist to sell")
+                    pass
--- backtest/strategy.py
+++ backtest/strategy.py
@@ -1,13 +1,10 @@
 import pandas as pd
-
+from .portfolio import Portfolio
+from typing import Literal
 class Strategy():
     def __init__(self):
         pass
 
-    def check_condition(self, event: tuple) -> bool:
+    def check_condition(self, event: tuple) -> Literal["BUY", "SELL", "HOLD"]:
         """Override this for each individual subclass"""
         raise NotImplementedError("check_condition must be implemented by subclass.")
-
-    def execute(self, event: tuple):
-        """Override this for each individual subclass"""
-        raise NotImplementedError("execute must be implemented by subclass.")
