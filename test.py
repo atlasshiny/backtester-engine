dd3146a677e193ac3f6bc5972087ca0a9f39942c
AtlasShiny <Mannyoso07@gmail.com>
2025-12-25 16:07:31 -0500

    another full successful backtest

--- test.py
+++ test.py
@@ -1,26 +1,30 @@
 from backtest import BacktestEngine
 from backtest import Strategy
+from backtest import Portfolio
 import pandas as pd
+import cProfile
 
 # strategy for testing
 class TestStrategy(Strategy):
     def __init__(self):
         super().__init__()
 
-    def check_condition(self, event: tuple) -> bool: 
+    def check_condition(self, event: tuple): 
         # assuming that event follows the structure in synthetic.csv
-        if event[6] > 300:
-            return True
+        if event[7] > 500:
+            return ["SELL",1]
+        elif event[7] < 250:
+            return ["BUY",4]
         else:
-            return False
-
-    def execute(self, event):
-        return "Would-be execution"
+            return "HOLD"
 
 strat = TestStrategy()
+account = Portfolio(120000.62)
 
 # turn the data set csv into a dataframe
 data_set = pd.read_csv("./data/synthetic.csv")
 
-engine = BacktestEngine(strategy=strat, data_set=data_set)
+engine = BacktestEngine(strategy=strat, portfolio=account, data_set=data_set)
 engine.run()
+engine.results(False)
+# cProfile.run('engine.run()', sort='cumtime')
