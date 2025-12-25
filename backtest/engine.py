b87c944485f1781d60ba639e007bf635a2d9b216
AtlasShiny <Mannyoso07@gmail.com>
2025-12-25 13:08:44 -0500

    refactored some of the code in engine.py to portfolio.py

--- backtest/engine.py
+++ backtest/engine.py
@@ -13,28 +13,12 @@ class BacktestEngine():
 
     def run(self):
         for event in self.data_set.itertuples():
-            print(event)
-            print("position count")
-            print(self.portfolio.position)
-            print("cash")
-            print(self.portfolio.cash)
-            match self.strategy.check_condition(event=event):
-                case "BUY":
-                    if self.portfolio.cash > event[3]: # check if there is enough money to buy (this is checking the "high" of the day to make the trade)
-                        self.portfolio.cash = self.portfolio.cash - event[3]
-                        self.portfolio.position += 1 # keep a simple count of positions, no information yet
-                        print("Action executed : Buying")
-                    else:
-                        print("No Action taken : Not enough money to buy in")
-                    pass
-                case "HOLD":
-                    print("Action executed : Holding")
-                    pass
-                case "SELL":
-                    if self.portfolio.position >= 1: # check if there is any positions to sell
-                        self.portfolio.position -= 1 # remove 1 from the count of positions, add some random amount of money in order for the system to work for testing purposes
-                        self.portfolio.cash += 100
-                        print("Action executed : Selling")
-                    else:
-                        print("No Action taken : No positions exist to sell")
-                    pass
+            Portfolio.execute(event=event, action=self.strategy.check_condition(event=event))
+                
+    def results(self, save: bool):
+        # have all other code run before saving
+        if save == True:
+            print("Enter saved results file name:")
+            file_name = input("")
+            # figure out how the data will be represented before trying to save a .txt or .csv file of it
+        pass
--- backtest/portfolio.py
+++ backtest/portfolio.py
@@ -1,10 +1,21 @@
+from typing import Literal
+
 class Portfolio():
     def __init__(self, initial_cash: float):
         self.cash = initial_cash
-        self.position = 0 # change to a dict or list eventually
+        self.position = {} # change to a dict or list eventually
         self.trade_log = []
         pass
 
-    def execute(self, event: tuple):
-        """Override this for each individual subclass"""
-        raise NotImplementedError("execute must be implemented by subclass.")
+    def execute(self, event: tuple, action: Literal["BUY", "SELL", "HOLD"]):
+        match action:
+            case "BUY":
+                if self.cash > event[3]: # check if there is enough money to buy (this is checking the "high" of the day to make the trade)
+                    self.cash = self.cash - event[3]
+                    # add the payload to be added to self.position
+            case "HOLD":
+                pass
+            case "SELL":
+                if self.position >= 1:
+                    self.cash += 105 # arbitrary value for now
+                    # add the logic to remove positions from self.position
