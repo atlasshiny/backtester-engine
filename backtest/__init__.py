69f4cb99688e4f1300b48307271ff342fd34ba5b
AtlasShiny <Mannyoso07@gmail.com>
2025-12-28 16:57:33 -0500

    exposed PerformanceAnalytics for module imports

--- backtest/__init__.py
+++ backtest/__init__.py
@@ -2,3 +2,4 @@ from .engine import BacktestEngine
 from .strategy import Strategy
 from .portfolio import Portfolio
 from .order import Order
+from .performance_analytics import PerformanceAnalytics
