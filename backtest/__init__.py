3d1efc01d85afb983cbdf2a5ed06413353a945b5
AtlasShiny <Mannyoso07@gmail.com>
2025-12-29 17:02:22 -0500

    added broker to the import list and a description

--- backtest/__init__.py
+++ backtest/__init__.py
@@ -1,6 +1,21 @@
+"""
+The backtest module provides core classes and logic for running trading strategy backtests,
+including the engine, broker, portfolio, order, analytics, and strategy interfaces.
+
+Initalization Order
+- Strategy
+- Portfolio
+- Broker
+- Engine
+
+After initalization, use engine.run() and engine.results() to run the backtest and get statistics
+
+"""
+
 from .engine import BacktestEngine
 from .strategy import Strategy
 from .portfolio import Portfolio
+from .broker import Broker
 from .performance_analytics import PerformanceAnalytics
 from .order import Order
 from .position import Position
