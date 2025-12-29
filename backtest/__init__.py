b6c7be82fb5bffde9083fc5b85ed49bbec33f9f7
AtlasShiny <Mannyoso07@gmail.com>
2025-12-28 22:08:17 -0500

    organizational change

--- backtest/__init__.py
+++ backtest/__init__.py
@@ -1,7 +1,6 @@
 from .engine import BacktestEngine
 from .strategy import Strategy
 from .portfolio import Portfolio
-from .order import Order
 from .performance_analytics import PerformanceAnalytics
 
 # dataclasses
